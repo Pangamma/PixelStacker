96c299f8549195947c2440cdb6f34ad002d67f41
pangamma <pro.tayloralove@gmail.com>
2020-01-05 16:42:51 -0800

    - links load faster - remove dead code

--- src/PixelStacker/UI/AboutForm.Designer.cs
+++ src/PixelStacker/UI/AboutForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-            this.lnkLumenGaming = new System.Windows.Forms.LinkLabel();
+            this.lnkMyWebsite = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
             this.lnkFontMaker = new System.Windows.Forms.LinkLabel();
             this.lnkWoolcityProject = new System.Windows.Forms.LinkLabel();
@@ -38,32 +38,35 @@
             this.lnkDownload = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // lnkLumenGaming
+            // lnkMyWebsite
             // 
-            this.lnkLumenGaming.AutoSize = true;
-            this.lnkLumenGaming.Location = new System.Drawing.Point(23, 241);
-            this.lnkLumenGaming.Name = "lnkLumenGaming";
-            this.lnkLumenGaming.Size = new System.Drawing.Size(117, 13);
-            this.lnkLumenGaming.TabIndex = 0;
-            this.lnkLumenGaming.TabStop = true;
-            this.lnkLumenGaming.Text = "LumenGaming Website";
-            this.lnkLumenGaming.Click += new System.EventHandler(this.lnkLumenGaming_Click);
+            this.lnkMyWebsite.AutoSize = true;
+            this.lnkMyWebsite.Location = new System.Drawing.Point(31, 297);
+            this.lnkMyWebsite.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lnkMyWebsite.Name = "lnkMyWebsite";
+            this.lnkMyWebsite.Size = new System.Drawing.Size(77, 17);
+            this.lnkMyWebsite.TabIndex = 0;
+            this.lnkMyWebsite.TabStop = true;
+            this.lnkMyWebsite.Text = "My website";
+            this.lnkMyWebsite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkMyWebsite_LinkClicked);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Location = new System.Drawing.Point(31, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(404, 169);
+            this.label1.Size = new System.Drawing.Size(545, 221);
             this.label1.TabIndex = 1;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // lnkFontMaker
             // 
             this.lnkFontMaker.AutoSize = true;
-            this.lnkFontMaker.Location = new System.Drawing.Point(187, 213);
+            this.lnkFontMaker.Location = new System.Drawing.Point(249, 262);
+            this.lnkFontMaker.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkFontMaker.Name = "lnkFontMaker";
-            this.lnkFontMaker.Size = new System.Drawing.Size(61, 13);
+            this.lnkFontMaker.Size = new System.Drawing.Size(79, 17);
             this.lnkFontMaker.TabIndex = 2;
             this.lnkFontMaker.TabStop = true;
             this.lnkFontMaker.Text = "Font Maker";
@@ -72,9 +75,10 @@
             // lnkWoolcityProject
             // 
             this.lnkWoolcityProject.AutoSize = true;
-            this.lnkWoolcityProject.Location = new System.Drawing.Point(23, 213);
+            this.lnkWoolcityProject.Location = new System.Drawing.Point(31, 262);
+            this.lnkWoolcityProject.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkWoolcityProject.Name = "lnkWoolcityProject";
-            this.lnkWoolcityProject.Size = new System.Drawing.Size(128, 13);
+            this.lnkWoolcityProject.Size = new System.Drawing.Size(169, 17);
             this.lnkWoolcityProject.TabIndex = 3;
             this.lnkWoolcityProject.TabStop = true;
             this.lnkWoolcityProject.Text = "Visit the Wool City Project";
@@ -83,9 +87,10 @@
             // lnkLinkedIn
             // 
             this.lnkLinkedIn.AutoSize = true;
-            this.lnkLinkedIn.Location = new System.Drawing.Point(187, 241);
+            this.lnkLinkedIn.Location = new System.Drawing.Point(249, 297);
+            this.lnkLinkedIn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkLinkedIn.Name = "lnkLinkedIn";
-            this.lnkLinkedIn.Size = new System.Drawing.Size(87, 13);
+            this.lnkLinkedIn.Size = new System.Drawing.Size(115, 17);
             this.lnkLinkedIn.TabIndex = 4;
             this.lnkLinkedIn.TabStop = true;
             this.lnkLinkedIn.Text = "Taylor\'s LinkedIn";
@@ -94,9 +99,10 @@
             // lnkDonate
             // 
             this.lnkDonate.AutoSize = true;
-            this.lnkDonate.Location = new System.Drawing.Point(299, 213);
+            this.lnkDonate.Location = new System.Drawing.Point(399, 262);
+            this.lnkDonate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkDonate.Name = "lnkDonate";
-            this.lnkDonate.Size = new System.Drawing.Size(78, 13);
+            this.lnkDonate.Size = new System.Drawing.Size(103, 17);
             this.lnkDonate.TabIndex = 5;
             this.lnkDonate.TabStop = true;
             this.lnkDonate.Text = "Buy me a beer!";
@@ -105,9 +111,10 @@
             // lnkDownload
             // 
             this.lnkDownload.AutoSize = true;
-            this.lnkDownload.Location = new System.Drawing.Point(299, 241);
+            this.lnkDownload.Location = new System.Drawing.Point(399, 297);
+            this.lnkDownload.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkDownload.Name = "lnkDownload";
-            this.lnkDownload.Size = new System.Drawing.Size(99, 13);
+            this.lnkDownload.Size = new System.Drawing.Size(128, 17);
             this.lnkDownload.TabIndex = 6;
             this.lnkDownload.TabStop = true;
             this.lnkDownload.Text = "Download Location";
@@ -115,18 +122,19 @@
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 278);
+            this.ClientSize = new System.Drawing.Size(605, 342);
             this.Controls.Add(this.lnkDownload);
             this.Controls.Add(this.lnkDonate);
             this.Controls.Add(this.lnkLinkedIn);
             this.Controls.Add(this.lnkWoolcityProject);
             this.Controls.Add(this.lnkFontMaker);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.lnkLumenGaming);
+            this.Controls.Add(this.lnkMyWebsite);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
@@ -138,7 +146,7 @@
 
         #endregion
 
-        private System.Windows.Forms.LinkLabel lnkLumenGaming;
+        private System.Windows.Forms.LinkLabel lnkMyWebsite;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel lnkFontMaker;
         private System.Windows.Forms.LinkLabel lnkWoolcityProject;
--- src/PixelStacker/UI/AboutForm.cs
+++ src/PixelStacker/UI/AboutForm.cs
@@ -30,25 +30,19 @@ namespace PixelStacker.UI
 
         private void lnkFontMaker_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            ProcessStartInfo sInfo = new ProcessStartInfo("https://mvc.lumengaming.com/v/fontmaker");  // Adf.ly
-            Process.Start(sInfo);
-        }
-
-        private void lnkLumenGaming_Click(object sender, EventArgs e)
-        {
-            ProcessStartInfo sInfo = new ProcessStartInfo("https://mvc.lumengaming.com/v/lg");  // Adf.ly
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://taylorlove.info/fontmaker/");  // Adf.ly
             Process.Start(sInfo);
         }
 
         private void lnkWoolcityProject_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            ProcessStartInfo sInfo = new ProcessStartInfo("https://mvc.lumengaming.com/v/wc_project");  // Adf.ly
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://lumengaming.com/index.php?threads/all-about-the-woolcity-project-freebuild.125/#post-427");  // Adf.ly
             Process.Start(sInfo);
         }
 
         private void lnkLinkedIn_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            ProcessStartInfo sInfo = new ProcessStartInfo("https://mvc.lumengaming.com/v/linkedin");  // Adf.ly
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://www.linkedin.com/in/tayloralove");  // Adf.ly
             Process.Start(sInfo);
         }
 
@@ -61,6 +55,12 @@ namespace PixelStacker.UI
         private void lnkDownload_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             ProcessStartInfo sInfo = new ProcessStartInfo("https://www.spigotmc.org/resources/pixelstacker.46812/");  // Adf.ly
+            Process.Start(sInfo);
+        }
+
+        private void lnkMyWebsite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://taylorlove.info");  // Adf.ly
             Process.Start(sInfo);
         }
     }
