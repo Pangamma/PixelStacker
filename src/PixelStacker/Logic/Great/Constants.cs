d3450fcf72c9ba520df30ab2131e6ad163dfaa3c
pangamma <pro.tayloralove@gmail.com>
2021-03-21 16:36:53 -0700

    Update world height

--- src/PixelStacker/Logic/Great/Constants.cs
+++ src/PixelStacker/Logic/Great/Constants.cs
@@ -8,7 +8,7 @@ namespace PixelStacker.Logic
 {
     public static class Constants
     {
-        public const string Version = "1.16.5c";
+        public const string Version = "1.16.5d";
         public const string Website = "https://taylorlove.info/pixelstacker";
         public const int TextureSize = 16;
         public const string RenderedZIndexFilter = "RenderedZIndexFilter";
@@ -17,7 +17,7 @@ namespace PixelStacker.Logic
         public const float MAX_ZOOM = 200F;
         public const float MIN_ZOOM = 200F;
         public const int SMALL_IMAGE_DIVIDE_SIZE = 2;
-        public const int WORLD_HEIGHT = 320;
+        public const int WORLD_HEIGHT = 384;
         public const int BIG_IMG_MAX_AREA_B4_SPLIT = 100000; // split image into smaller sizes if bigger than this
 
         // Data version of MC build. Can be found on wiki.
